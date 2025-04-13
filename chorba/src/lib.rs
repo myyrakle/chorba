33c142df90b1b1272abe57fb764a26a3f693640d
myyrakle <sssang97@naver.com>
2025-04-14 02:29:49 +0900

    derive_encode 구현

--- chorba/src/lib.rs
+++ chorba/src/lib.rs
@@ -1,4 +1,7 @@
 pub mod decode;
 pub mod encode;
 
+pub use decode::Decoder;
+pub use encode::Encoder;
+
 pub const LENGTH_TAG_BYTES: usize = 4;
--- chorba_macro/src/lib.rs
+++ chorba_macro/src/lib.rs
@@ -1,1 +1,36 @@
+#[proc_macro_derive(Encode)]
+pub fn derive_encode(item: TokenStream) -> TokenStream {
+    let mut new_code = "".to_string();
 
+    let ast = syn::parse_macro_input!(item as syn::ItemStruct);
+
+    let struct_name = ast.ident.to_string();
+
+    new_code += format!(r#"impl chorba::Encoder for {struct_name} {{"#).as_str();
+
+    {
+        new_code += r#"fn encode(&self) -> Vec<u8> {"#;
+
+        {
+            new_code += r#"let mut buffer = Vec::new();"#;
+
+            let mut need_comma = false;
+
+            for field in ast.fields.iter() {
+                let field_name = field.ident.as_ref().unwrap().to_string();
+
+                new_code +=
+                    format!(r#"let bytes = chorba::Encoder::encode(self.{field_name});"#).as_str();
+                new_code += format!(r#"buffer.extend(bytes);"#).as_str();
+            }
+
+            new_code += r#"buffer"#;
+        }
+
+        new_code += r#"}"#;
+    }
+
+    new_code += "}";
+
+    return TokenStream::from_str(new_code.as_str()).unwrap();
+}
