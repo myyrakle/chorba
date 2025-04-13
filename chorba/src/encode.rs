63d2adbf91083df47f415bf1203b713768f76eab
myyrakle <sssang97@naver.com>
2025-04-14 02:06:38 +0900

    Encoder 트레잇 구현: bytes

--- chorba/src/encode.rs
+++ chorba/src/encode.rs
@@ -4,6 +4,24 @@ pub trait Encoder {
     fn encode(&self) -> Vec<u8>;
 }
 
+impl Encoder for String {
+    fn encode(&self) -> Vec<u8> {
+        serialize_bytes_with_length(self.as_bytes())
+    }
+}
+
+impl Encoder for Vec<u8> {
+    fn encode(&self) -> Vec<u8> {
+        serialize_bytes_with_length(self)
+    }
+}
+
+impl Encoder for &[u8] {
+    fn encode(&self) -> Vec<u8> {
+        serialize_bytes_with_length(self)
+    }
+}
+
 pub fn serialize_bytes_with_length(bytes: &[u8]) -> Vec<u8> {
     let mut result = Vec::with_capacity(bytes.len() + LENGTH_TAG_BYTES);
 
