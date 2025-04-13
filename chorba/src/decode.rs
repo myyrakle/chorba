695b8da18a40ce986ff6d8a02b24e10793d7717c
myyrakle <sssang97@naver.com>
2025-04-14 02:03:52 +0900

    deserialize 구현

--- chorba/src/decode.rs
+++ chorba/src/decode.rs
@@ -1,3 +1,5 @@
+use crate::LENGTH_TAG_BYTES;
+
 #[derive(thiserror::Error, Debug)]
 pub enum DecodeError {
     #[error("Invalid data length")]
@@ -10,4 +12,25 @@ pub enum DecodeError {
 
 pub trait Decoder<T: Sized> {
     fn decode(data: &[u8]) -> Result<T, DecodeError>;
+}
+
+pub fn deserialize(bytes: &[u8]) -> Option<(&[u8], &[u8])> {
+    if bytes.len() < LENGTH_TAG_BYTES {
+        return None;
+    }
+
+    let length = u32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]) as usize;
+
+    // cut length tag
+    let bytes = &bytes[LENGTH_TAG_BYTES..];
+
+    if bytes.len() < length {
+        return None;
+    }
+
+    let current_bytes = &bytes[..length];
+
+    let rest_bytes = &bytes[length..];
+
+    Some((current_bytes, rest_bytes))
 }
